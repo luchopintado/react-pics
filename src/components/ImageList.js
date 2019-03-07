d83f111526de93e821e0313597fbf105c6333ad9
Luis Pintado <lpintado@belatrixsf.com>
2019-03-07 15:14:55 -0500

    version 2

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-    const images = props.images.map(({ id, description, urls }) => {
-        return (
-            <div key={id}>
-                <img src={urls.regular} alt={description} />;
-            </div>
-        );
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />;
     });
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
