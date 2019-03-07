38e56f788f0b0ba41d17ca493aee2a7e989aa59d
Luis Pintado <lpintado@belatrixsf.com>
2019-03-07 14:23:58 -0500

    version 1

---
+++ src/components/ImageList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    const images = props.images.map(({ id, description, urls }) => {
+        return (
+            <div key={id}>
+                <img src={urls.regular} alt={description} />;
+            </div>
+        );
+    });
+    return <div>{images}</div>;
+};
+
+export default ImageList;
